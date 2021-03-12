553697e7e96b33cf0a56bf233d84546a556a1451
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2021-03-11 18:34:47 -0700

    Mass Update (cleaner code)

---
+++ BotRun.bat
@@ -0,0 +1,3 @@
+@echo off
+python C:\Users\coopg\Documents\Code\steam-bot\ss-autobot.py %*
+pause
--- ss-autobot.py
+++ ss-autobot.py
@@ -3,18 +3,19 @@
 # -----------------------------------------------
 from selenium import webdriver
 from time import sleep
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 
 # Gather user info:
 steamUser = input("Steam Username: ")
 steamPass = input("Steam Password: ")
 
 
+# Load page and begin loop to sell all cards
 class SteamBot:
     def __init__(self, user, password):
         # chromeDriver to open site:
         self.driver = webdriver.Chrome()
         self.driver.get("https://steamcommunity.com/login/home/")
+        self.driver.implicitly_wait(15)
         # Login Page
         self.driver.find_element_by_xpath("/html/body/div[1]/div[7]/div[2]/div[1]/div/div["
                                           "1]/div/div/div/div/form/div[1]/input") \
@@ -33,32 +34,28 @@ class SteamBot:
         self.driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/form/div[4]/div[1]/div[1]/div[1]") \
             .click()
         # Main account page
-        sleep(3)
         self.driver.find_element_by_xpath("//span[contains(text(),'Inventory')]") \
             .click()
-        sleep(2)
         # Inventory page
-        sleep(2)
         self.driver.find_element_by_xpath("//*[@id='inventory_link_753']/span[2]") \
             .click()
-        sleep(2)
+        sleep(5)
 
-        self.sellCard(False)
+        self.sell_card(False)
 
         # Continue to sell cards
         empty = False
         while not empty:
             try:
-                self.sellCard(True)
-            except NoSuchElementException:  # Replace with element not found error
+                self.sell_card(True)
+            except:
                 empty = True
-                print("Steam Inventory Cleared. Thank you for choosing the SS AUTOBOT.")
-            except ElementNotInteractableException:
-                empty = True
-                print("Steam Inventory Cleared. Thank you for choosing the SS AUTOBOT.")
+                print("Steam Inventory Cleared - 200")
+                print("If an error occurred and the inventory is not cleared, please submit an Issue VIA GitHub")
                 sleep(8)
 
-    def sellCard(self, itemboxChecked):
+    # Loop To sell cards
+    def sell_card(self, item_box_checked):
         sleep(2)
         self.driver.find_element_by_xpath("/html/body/div[1]/div[7]/div[2]/div[1]/div[2]/div/div[5]/div[4]/div["
                                           "3]/div[1]/span") \
@@ -96,7 +93,7 @@ class SteamBot:
             .send_keys(price)
         sleep(2)
         # Click terms checkbox
-        if not itemboxChecked:
+        if not item_box_checked:
             self.driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/div[4]/div[1]/div[3]/div/input") \
                 .click()
             sleep(2)
@@ -107,7 +104,6 @@ class SteamBot:
         # Click accept
         self.driver.find_element_by_xpath("//*[@id='market_sell_dialog_ok']/span") \
             .click()
-        sleep(3)
 
 
 SteamBot(steamUser, steamPass)
