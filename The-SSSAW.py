567105d23351e4c3ace757c46d603fc01f7b6a83
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2022-01-31 15:33:21 -0700

    Optimized. Ready for use

--- The-SSSAW.py
+++ The-SSSAW.py
@@ -2,94 +2,74 @@
 # SS AUTOBOT
 # -----------------------------------------------
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.remote.remote_connection import LOGGER
+import logging
 from time import sleep
 
 # Gather user info:
-steamUser = input("Steam Username: ")
-steamPass = input("Steam Password: ")
 selection = int(input("How many cards do you want to sell?: "))
 
 
 # Load page and begin loop to sell all cards
 class SteamBot:
-    def __init__(self, user, password):
+    def __init__(self):
         # chromeDriver to open site:
         self.driver = webdriver.Chrome()
         self.driver.get("https://steamcommunity.com/login/home/")
-        self.driver.implicitly_wait(15)
-        # Login Page
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[7]/div[2]/div[1]/div/div["
-                                          "1]/div/div/div/div/form/div[1]/input") \
-            .send_keys(user)
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[7]/div[2]/div[1]/div/div["
-                                          "1]/div/div/div/div/form/div[2]/input") \
-            .send_keys(password)
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[7]/div[2]/div[1]/div/div[1]/div/div/div/div/div["
-                                          "3]/div[1]/button/span") \
-            .click()
-        sleep(1)
-        # Steam Guard
-        steamAuth = input("Steam Guard Code: ")
-        self.driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/form/div[3]/div[1]/div/input") \
-            .send_keys(steamAuth)
-        self.driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/form/div[4]/div[1]/div[1]/div[1]") \
-            .click()
-        # Main account page
-        self.driver.find_element_by_xpath("//span[contains(text(),'Inventory')]") \
-            .click()
-        # Inventory page
-        self.driver.find_element_by_xpath("//*[@id='inventory_link_753']/span[2]") \
-            .click()
-        sleep(5)
 
-        self.sell_card(False)
+        # wait until user has opened the inventory
+        print("Please Enter your information and Navigate to the Steam Inventory page.")
+        WebDriverWait(self.driver, 60).until(expected_conditions.title_contains("Inventory"))
 
-        # Continue to sell cards
+        # main loop
+        self.sell_card(False) # run once to check terms box
         count = 1
         while count < selection:
             self.sell_card(True)
             count += 1
 
+        # end credits
         print(f"You sold {selection} cards!")
+        print("Made by CoopGod --> github.com/coopgod")
         sleep(8)
 
     # Loop To sell cards
     def sell_card(self, item_box_checked):
         sleep(2)
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[7]/div[2]/div[1]/div[2]/div/div[5]/div[4]/div["
-                                          "3]/div[1]/span") \
-            .click()
-        sleep(2)
+        # Steam catagory header
+        self.driver.find_element_by_xpath('//*[@id="inventory_link_753"]/span[3]').click()
+        # Advanced Options Menu Button
+        self.driver.find_element_by_xpath('//*[@id="filter_tag_show"]/span').click()
+        sleep(1)
         # Advanced Options Menu
         self.driver.find_element_by_xpath("//*[@id='tag_filter_753_0_misc_marketable']") \
             .click()
         self.driver.find_element_by_xpath("//input[@id='tag_filter_753_0_item_class_item_class_2']") \
             .click()
         # Select first card
-        self.driver.find_element_by_xpath("//body/div[1]/div[7]/div[2]/div[1]/div[2]/div[1]/div[5]/div["
-                                          "9]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/a[1]") \
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[7]/div[2]/div[1]/div[2]/div/div[5]/div[9]/div[2]/div[1]/div[1]/div[1]/div[3]/div/a") \
             .click()
         sleep(2)
         # Copy starting price
-        startingPrice = self.driver.find_elements_by_xpath('//*[@id="iteminfo0_item_market_actions"]/div/div[2]')
+        startingPrice = self.driver.find_elements_by_xpath('//*[@id="iteminfo1_item_market_actions"]/div/div[2]')[0].text
         # Get starting value
-        priceRaw = startingPrice[0].get_attribute('innerHTML')
-        priceRawArray = priceRaw.split(' ')
-        priceRawArray2 = priceRawArray[3].split('<br>')
-        price = priceRawArray2[0]
-
+        priceRawArray = startingPrice.split(' ')
+        priceToSell = priceRawArray[3].split('V')[0]
+        print(f"Selling for ${priceToSell}")
+        sleep(0.5)
         # Sell Cards button
-        self.driver.find_element_by_xpath('//*[@id="iteminfo0_item_market_actions"]/a/span[2]') \
+        self.driver.find_element_by_xpath('//*[@id="iteminfo1_item_market_actions"]/a/span[2]') \
             .click()
         sleep(2)
         # Type in starting price
         self.driver.find_element_by_xpath("//*[@id='market_sell_buyercurrency_input']") \
-            .send_keys(price)
+            .send_keys(priceToSell)
         sleep(2)
         # Click terms checkbox
         if not item_box_checked:
-            self.driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/div[4]/div[1]/div[3]/div/input") \
-                .click()
+            self.driver.find_element_by_xpath('//*[@id="market_sell_dialog_accept_ssa"]').click()
             sleep(2)
         # Click sell button
         self.driver.find_element_by_xpath("//*[@id='market_sell_dialog_accept']/span") \
@@ -100,4 +80,4 @@ class SteamBot:
             .click()
 
 
-SteamBot(steamUser, steamPass)
+SteamBot()
